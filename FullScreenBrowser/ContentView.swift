eee2121b4a98f3372b302c414b88e8da285d4ad2
Matthew Rutter <rutter.matt@ymail.com>
2020-08-23 21:27:32 -0400

    Initial Commit

---
+++ FullScreenBrowser/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FullScreenBrowser
+//
+//  Created by Matthew Rutter on 8/23/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ FullScreenBrowser/FullScreenBrowserApp.swift
@@ -0,0 +1,17 @@
+//
+//  FullScreenBrowserApp.swift
+//  FullScreenBrowser
+//
+//  Created by Matthew Rutter on 8/23/20.
+//
+
+import SwiftUI
+
+@main
+struct FullScreenBrowserApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
