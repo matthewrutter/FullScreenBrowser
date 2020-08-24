d44477552a96207ba381641338a0eeb557d3f5a3
Matthew Rutter <rutter.matt@ymail.com>
2020-08-23 23:32:54 -0400

    added webview structure and required functions

--- FullScreenBrowser/WebView.swift
+++ FullScreenBrowser/WebView.swift
@@ -7,3 +7,28 @@
 
 import SwiftUI
 import WebKit
+
+struct WebView: UIViewRepresentable {
+    var url: String //passed in when WebView is created
+    
+    func makeUIView(context: Context) -> WKWebView {
+        
+        //return the actual view were going to show
+        guard let url = URL(string: self.url) else {
+            
+            //return an empty view if the URL cant be created
+            return WKWebView()
+        }
+        
+        let request = URLRequest(url: url)
+        let wkWebView = WKWebView()
+        
+        wkWebView.load(request)
+        
+        return wkWebView
+    }
+    
+    func updateUIView(_ uiView: WKWebView, context: Context) {
+        //required but ignoring
+    }
+}
